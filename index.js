c3de129ca0413f663c05aab0f53144a3e358dfd2
Elizabeth Zevin <ehzevin@gmail.com>
2019-03-02 23:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.random()*100;
+  var itemObj = {itemName:item, itemPrice:Math.floor(price)};
+  cart.push(itemObj);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+ for (let i=cart.length; i<cart.length; i++);
+  if (cart.length>0)
+  {cart.push(`${[]} at $${Math.floor(Math.random()*100)}`);
+  return "In your cart, you have" +  cart + "."}
+  else (cart.length===0);
+  {return "Your shopping cart is empty."}
+  
 }
+
 
 function total() {
   // write your code here
